6b3fcdcdfc6c765d54e0bea93725495df27d8905
ii14 <ii14@users.noreply.github.com>
2023-09-15 03:36:46 +0200

    ci

--- test/test.cpp
+++ test/test.cpp
@@ -4,3 +4,15 @@ int main(int argc, char** argv)
 {
   return Catch::Session().run(argc, argv);
 }
+
+// NOLINTNEXTLINE(readability-identifier-naming, bugprone-reserved-identifier)
+extern "C" const char* __ubsan_default_options()
+{
+  return "print_stacktrace=1";
+}
+
+// NOLINTNEXTLINE(readability-identifier-naming, bugprone-reserved-identifier)
+extern "C" const char* __asan_default_options()
+{
+  return "detect_leaks=1,handle_abort=1,handle_sigill=1";
+}
