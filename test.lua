b02bc08e943f60d0114c8c9051aa92b5a581ce14
ii14 <ii14@users.noreply.github.com>
2023-11-06 15:02:07 +0100

    refactor: remove Makefile and test.lua

--- test.lua
+++
@@ -1,5 +0,0 @@
-local cwd = vim.fn.getcwd()
-vim.o.rtp = vim.o.rtp .. ',' .. cwd
-package.preload['fzxlib'] = function()
-  return package.loadlib(cwd .. '/build/release-gcc/lib/fzxlib.so', 'luaopen_fzxlib')()
-end
